d0a999826e1694b6c5b01facf55f9e75996f9041
XuanDatNguyen <nguyenxuandat.bqp@gmail.com>
2022-12-21 17:01:14 +0700

    update

--- ShopifyAPI/crud_product.py
+++ ShopifyAPI/crud_product.py
@@ -1,15 +1,68 @@
 import requests
 from settings import USERNAME, PASSWORD, SHOP, ADMIN_ACCESS_TOKEN
 
-base_url = F"https://{USERNAME}:{PASSWORD}@{SHOP}.myshopify.com/admin/api/2022-10/"
+base_url = F"https://{SHOP}.myshopify.com/admin/api/2022-10/"
 headers = {'Content-Type': 'application/json',
            'X-Shopify-Access-Token': ADMIN_ACCESS_TOKEN}
 
 
-def get_products():
-    url = F"{base_url}products.json"
-    res = requests.get(url, headers=headers)
+# def delete_product(product_id):
+#     url = F"{base_url}/products/{product_id}.json"
+#     res = requests.delete(url, headers=headers)
+#     return res.status_code
+
+
+# Đọc hiểu API về product, collection, customer, order.
+# Create:
+# Collection: 1 Smart collection, 1 Custom collection.
+# Product: 1 Product không option, 1 Product có 2 options (Size, Color,...).
+# Customer: 1 customer (có thể lấy thông tin chính bản thân).
+# Order: tạo 1 order cho customer khi mua product đã tạo bên trên.
+# Update: cập nhật giá, cập nhật ảnh, cập nhật số lương (qty) cho 2 product vừa tạo.
+# Delete: Xoá toàn bộ dữ liệu vừa tạo.
+
+
+# -----Smart collection-----
+def create_smart_collection(body):
+    url = F"{base_url}smart_collections.json"
+    res = requests.post(url, headers=headers, json=body)
     return res.text
+
+
+smart_body = {
+    "smart_collection": {
+        "title": "SSS Collections",
+        "body_html": "Bộ sưu tập đến từ  brand SSSTUTTER",
+        "rules": [
+            {
+                "column": "vendor",
+                "relation": "equals",
+                "condition": "SSSTUTTER"
+            }
+        ]
+    }
+}
+
+# print(create_smart_collection(smart_body))
+
+
+# -----Custom collection-----
+def create_custom_collection(body):
+    url = F"{base_url}/custom_collections.json"
+    res = requests.post(url, headers=headers, json=body)
+    return res.text
+
+
+custom_body = {
+    "custom_collection": {
+        "title": "Summer collections",
+        "body_html": "Bộ sưu tập mùa hè năm nay với những item vô cùng hot chắc chắn sẽ khiến bạn hài lòng"
+    }
+}
+
+# print(create_custom_collection(custom_body))
+
+# -----Product-----
 
 
 def create_product(data):
@@ -18,20 +71,105 @@ def create_product(data):
     return res.text
 
 
-def update_product(product_id, data):
+# Product without option
+product = {
+    "product": {
+        "title": "CITY SWEATSHIRT",
+        "vendor": "SSSTUTTER",
+        "product_type": "Sweter",
+        "status": "active",
+    }
+}
+
+
+# Product with option size & color
+product_o = {
+    "product": {
+        "title": "SMART PANTS",
+        "body_html": "<p>Quần âu may kèm chun ẩn, co giãn</p>\n<p>Dáng suông thẳng, ống quần crop nhẹ</p>\n",
+        "vendor": "SSSTUTTER",
+        "product_type": "Jeans",
+        "variants": [
+            {
+                "option1": "Be",
+                "option2": "0"
+            },
+            {
+                "option1": "Be",
+                "option2": "1"
+            },
+            {
+                "option1": "Đen",
+                "option2": "0"
+            },
+            {
+                "option1": "Đen",
+                "option2": "1"
+            }
+        ],
+        "options": [
+            {
+                "name": "Color",
+                "values": ["Be", "Đen"]
+            },
+            {
+                "name": "Size",
+                "values": [0, 1]
+            }
+        ]
+    }
+}
+print(create_product(product_o))
+
+# 8081258709265, 8081380180241
+
+update_product = {
+    "product": {
+        "images": [
+            {
+                "position": 1,
+                "src": "https://cdn.ssstutter.com/products/po0EUQXd52Ks47dT/112022/1667904313230.jpeg"
+            }
+        ]
+    }
+}
+
+
+def update_product(product_id, body):
     url = F"{base_url}/products/{product_id}.json"
-    res = requests.put(url, headers=headers, json=data)
+    res = requests.put(url, headers=headers, json=body)
+    return res.text
+# -----Customer-----
+
+
+def create_customer(body):
+    url = F"{base_url}customers.json"
+    res = requests.post(url, json=body, headers=headers)
     return res.text
 
 
-def delete_product(product_id):
-    url = F"{base_url}/products/{product_id}.json"
+def delete_customer(customer_id):
+    url = F"{base_url}customers/{customer_id}.json"
     res = requests.delete(url, headers=headers)
     return res.status_code
 
 
-# print(delete_product(8078773420305))8078790820113
-# print(get_products())
-data = {"product": {"title": "Test product"}}
-print(create_product(data))
-# print(update_product(8078790820113, data))
+customer = {
+    "customer": {
+        "first_name": "Nguyen Xuan",
+        "last_name": "Dat",
+        "email": "xuandatnguyen27@gmail.com",
+        "phone": "0372545906",
+        "verified_email": True,
+        "addresses": [
+            {
+                "address1": "103a8, Khương Trung, Thanh Xuân, Hà Nội",
+                "city": "Ha Noi",
+                "country": "VN"
+            }
+        ]
+    }
+}
+# print(create_customer(customer))
+# print(delete_customer(6714770358545))
+# -----Order-----#
