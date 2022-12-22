5e3a7d0c4f6170ea8fc1856ffaed9008c75dd992
XuanDatNguyen <nguyenxuandat.bqp@gmail.com>
2022-12-22 14:05:21 +0700

    update

--- ShopifyAPI/crud_product.py
+++ ShopifyAPI/crud_product.py
@@ -1,28 +1,16 @@
 import requests
-from settings import USERNAME, PASSWORD, SHOP, ADMIN_ACCESS_TOKEN
+
+# Shop name
+SHOP = "james-5030"
+# Admin API access token
+ADMIN_ACCESS_TOKEN = "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"
 
 base_url = F"https://{SHOP}.myshopify.com/admin/api/2022-10/"
 headers = {'Content-Type': 'application/json',
            'X-Shopify-Access-Token': ADMIN_ACCESS_TOKEN}
 
 
-# def delete_product(product_id):
-#     url = F"{base_url}/products/{product_id}.json"
-#     res = requests.delete(url, headers=headers)
-#     return res.status_code
-
-
-# Đọc hiểu API về product, collection, customer, order.
-# Create:
-# Collection: 1 Smart collection, 1 Custom collection.
-# Product: 1 Product không option, 1 Product có 2 options (Size, Color,...).
-# Customer: 1 customer (có thể lấy thông tin chính bản thân).
-# Order: tạo 1 order cho customer khi mua product đã tạo bên trên.
-# Update: cập nhật giá, cập nhật ảnh, cập nhật số lương (qty) cho 2 product vừa tạo.
-# Delete: Xoá toàn bộ dữ liệu vừa tạo.
-
-
-# -----Smart collection-----
+# Tạo mới smart collection
 def create_smart_collection(body):
     url = F"{base_url}smart_collections.json"
     res = requests.post(url, headers=headers, json=body)
@@ -42,11 +30,10 @@ smart_body = {
         ]
     }
 }
-
 # print(create_smart_collection(smart_body))
 
 
-# -----Custom collection-----
+# Tạo mới custom collection
 def create_custom_collection(body):
     url = F"{base_url}/custom_collections.json"
     res = requests.post(url, headers=headers, json=body)
@@ -62,16 +49,15 @@ custom_body = {
 
 # print(create_custom_collection(custom_body))
 
-# -----Product-----
 
-
+# Tạo mới sản phẩm
 def create_product(data):
     url = F"{base_url}products.json"
     res = requests.post(url, json=data, headers=headers)
     return res.text
 
 
-# Product without option
+# Sản phẩm không có option
 product = {
     "product": {
         "title": "CITY SWEATSHIRT",
@@ -81,8 +67,7 @@ product = {
     }
 }
 
-
-# Product with option size & color
+# Sản phẩm có option
 product_o = {
     "product": {
         "title": "SMART PANTS",
@@ -92,19 +77,23 @@ product_o = {
         "variants": [
             {
                 "option1": "Be",
-                "option2": "0"
+                "option2": "0",
+                "inventory_management": "shopify",
             },
             {
                 "option1": "Be",
-                "option2": "1"
+                "option2": "1",
+                "inventory_management": "shopify",
             },
             {
                 "option1": "Đen",
-                "option2": "0"
+                "option2": "0",
+                "inventory_management": "shopify",
             },
             {
                 "option1": "Đen",
-                "option2": "1"
+                "option2": "1",
+                "inventory_management": "shopify",
             }
         ],
         "options": [
@@ -119,35 +108,108 @@ product_o = {
         ]
     }
 }
-print(create_product(product_o))
 
-# 8081258709265, 8081380180241
+# print(create_product(product_o))
 
-update_product = {
+
+# Cập nhật sản phẩm
+def update_product(product_id, body):
+    url = F"{base_url}products/{product_id}.json"
+    res = requests.put(url, headers=headers, json=body)
+    return res.text
+
+
+# Update hình ảnh, giá, qty sản phẩm không có option
+product_update = {
     "product": {
+        "variants": [
+            {
+                "price": 356150,
+                "compare_at_price": 419000,
+                "inventory_quantity": 10,
+            }
+        ],
         "images": [
             {
-                "position": 1,
                 "src": "https://cdn.ssstutter.com/products/po0EUQXd52Ks47dT/112022/1667904313230.jpeg"
+            },
+            {
+                "src": "https://cdn.ssstutter.com/products/po0EUQXd52Ks47dT/112022/1667804906083.jpeg"
+            },
+            {
+                "src": "https://cdn.ssstutter.com/products/po0EUQXd52Ks47dT/122022/1671002395327.jpeg"
             }
         ]
     }
 }
 
+# Update hình ảnh, giá sản phẩm có option
+product_update_o = {
+    "product": {
+        "variants": [
+            {
+                "id": 44250099319057,
+                "price": 356150,
+                "compare_at_price": 419000,
+            },
+            {
+                "id": 44250099351825,
+                "price": 356150,
+                "compare_at_price": 419000,
+            },
+            {
+                "id": 44250099384593,
+                "price": 356150,
+                "compare_at_price": 419000,
+            },
+            {
+                "id": 44250099417361,
+                "price": 356150,
+                "compare_at_price": 419000,
+            }
+        ],
+        "images": [
+            {
+                "position": 1,
+                "src": "https://cdn.ssstutter.com/products/po0EUQXd52Ks47dT/062022/1655042533957.jpeg"
+            },
+            {
+                "position": 2,
+                "src": "https://cdn.ssstutter.com/products/po0EUQXd52Ks47dT/062022/1655042573549.jpeg"
+            },
+        ]
+    }
+}
 
-def update_product(product_id, body):
-    url = F"{base_url}/products/{product_id}.json"
-    res = requests.put(url, headers=headers, json=body)
+product_id = 8083354550545
+# print(update_product(product_id, product_update_o))
+
+
+# Điều chỉnh số lượng sản phẩm có option
+def adjust_inventory_qty(location_id, inventory_item_id, available_adjustment):
+    url = F"{base_url}/inventory_levels/adjust.json"
+    body = {"location_id": location_id, "inventory_item_id": inventory_item_id,
+            "available_adjustment": available_adjustment}
+    res = requests.post(url, headers=headers, json=body)
     return res.text
-# -----Customer-----
+# print(adjust_inventory_qty(74947690769, 46298484277521, 5))
 
 
+# Xóa sản phẩm
+def delete_product(product_id):
+    url = F"{base_url}/products/{product_id}.json"
+    res = requests.delete(url, headers=headers)
+    return res.status_code
+
+
+# Tạo mới khách hàng
 def create_customer(body):
     url = F"{base_url}customers.json"
     res = requests.post(url, json=body, headers=headers)
     return res.text
 
 
+# Xóa khách hàng
 def delete_customer(customer_id):
     url = F"{base_url}customers/{customer_id}.json"
     res = requests.delete(url, headers=headers)
@@ -172,4 +234,35 @@ customer = {
 }
 # print(create_customer(customer))
 # print(delete_customer(6714770358545))
-# -----Order-----#
+
+
+# Tạo mới order
+def create_order(customer_id, variant_id, qty):
+    url = F"{base_url}orders.json"
+    body = {
+        "order": {
+            "line_items": [
+                {
+                    "variant_id": variant_id,
+                    "quantity": qty
+                }
+            ],
+            "customer": {
+                "id": customer_id
+            },
+            "financial_status": "paid"
+        }
+    }
+    res = requests.post(url, headers=headers, json=body)
+    return res.text
+# print(create_order(1, 2, 1))
+
+
+# Xóa order
+def delete_order(order_id):
+    url = F"{base_url}orders/{order_id}.json"
+    res = requests.delete(url, headers=headers)
+    return res.status_code
+
+
+print(delete_order(5222835454225))
